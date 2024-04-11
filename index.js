1cd04e9f4fb3473ad2d73d20f456326bb4bbaf17
Anastasiya Kubatina <82117054+kybikn@users.noreply.github.com>
2024-04-11 17:22:02 +0300

    refactor: чистка кода

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const btnPrev = document.querySelector(".slider_arrow-left");
 const btnNext = document.querySelector(".slider_arrow-right");
 
-
 const slides = document.querySelectorAll(".cards_item");
 const dots = document.querySelectorAll(".slider-square");
 
@@ -57,43 +56,6 @@ dots.forEach((item, indexDot) => {
         currentSlide(index);
     });
 });
-
-// --------------слайдер 1366----------------
-// const carousel = document.querySelector(".carousel");
-// const petsArrowLeft = document.querySelector(".slider_arrow-left");
-// const petsArrowRight = document.querySelector(".slider_arrow-right");
-
-// const cardsLeft = document.querySelector("#cards-left");
-// const cardsRight = document.querySelector("#cards-right");
-// const cardsActive = document.querySelector("#cards-active");
-
-// function moveRight() {
-//     carousel.classList.add("transition-right");
-//     petsArrowLeft.removeEventListener("click", moveLeft);
-//     petsArrowRight.removeEventListener("click", moveRight);
-// }
-// function moveLeft() {
-//     carousel.classList.add("transition-left");
-//     petsArrowLeft.removeEventListener("click", moveLeft);
-//     petsArrowRight.removeEventListener("click", moveRight);
-// }
-
-// function animationEnd(animationEvent) {
-//     let changedItem;
-//     if (animationEvent.animationName === "move-left") {
-//         carousel.classList.remove("transition-left");
-//         changedItem = cardsLeft;
-//         cardsActive.innerHTML = cardsLeft.innerHTML;
-//     } else {
-//         carousel.classList.remove("transition-right");
-//         changedItem = cardsRight;
-//         cardsActive.innerHTML = cardsRight.innerHTML;
-//     }
-// }
-// petsArrowLeft.addEventListener("click", moveLeft);
-// petsArrowRight.addEventListener("click", moveRight);
-// carousel.addEventListener("animationend", animationEnd);
-// --------------
 
 btnNext.addEventListener("click", nextSlide);
 btnPrev.addEventListener("click", prevSlide);
