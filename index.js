20b2439f61c0e452f5b18f76dd5ed796f4904a46
Anastasiya Kubatina <82117054+kybikn@users.noreply.github.com>
2024-04-12 01:17:51 +0300

    feat: сделан слайдер participants

--- index.js
+++ index.js
@@ -9,43 +9,79 @@ const totalPages = document.querySelector(".page-number.second");
 totalPages.textContent = `${slides.length}`;
 
 let index = 0;
+let page = 1;
 
+// количество одновременных карточек на экране
+let cardsOnScreen = 3;
+function updateCardsOnScreen() {
+    let width = window.screen.availWidth;
+    if (width <= 375) {
+        cardsOnScreen = 1
+    } else if (width <= 800) {
+        cardsOnScreen = 2
+    }
+    else {
+        cardsOnScreen = 3
+    };
+}
+
+// отображение карточек
 function activeSlide(n) {
-    for (slide of slides) {
+    for (let slide of slides) {
         slide.classList.remove("active");
     }
     slides[n].classList.add("active");
+    if ((cardsOnScreen > 1) && ((n + 1) < slides.length)) {
+        slides[n + 1].classList.add("active");
+    }
+    if ((cardsOnScreen > 2) && ((n + 2) < slides.length)) {
+        slides[n + 2].classList.add("active");
+    }
 }
-
+// отображение точек
 function activeDot(n) {
-    for (dot of dots) {
+    for (let dot of dots) {
         dot.classList.remove("active");
     }
     dots[n].classList.add("active");
 }
 
+// отображение страниц
+function activePage(n) {
+    page = Math.ceil((n + 1) / cardsOnScreen);
+    currentPage.innerText = (page);
+    const totalPagesNum = Math.ceil((slides.length) / cardsOnScreen);
+    totalPages.innerText = totalPagesNum
+}
+
+// обновляет ситуацию на экране
 function currentSlide(n) {
     activeSlide(n);
-    activeDot(n);
-    currentPage.innerText = (index + 1);
+    activePage(n);
+}
+
+// при загрузке
+function onLoad() {
+    updateCardsOnScreen();
+    currentSlide(0);
 }
 
 function nextSlide() {
-    if (index == slides.length - 1) {
+    index = index + cardsOnScreen;
+    if (index >= slides.length) {
         index = 0;
         currentSlide(index);
     } else {
-        index++;
         currentSlide(index);
     }
 }
 
 function prevSlide() {
-    if (index == 0) {
-        index = slides.length - 1;
+    index = index - cardsOnScreen;
+    if (index < 0) {
+        index = (Math.ceil((slides.length) / cardsOnScreen) - 1) * cardsOnScreen;
         currentSlide(index);
     } else {
-        index--;
         currentSlide(index);
     }
 }
@@ -60,4 +96,5 @@ dots.forEach((item, indexDot) => {
 btnNext.addEventListener("click", nextSlide);
 btnPrev.addEventListener("click", prevSlide);
 
-window.addEventListener("load");
+window.addEventListener("load", onLoad);
+window.addEventListener("resize", updateCardsOnScreen);
